5f559904f897272ccfb0d9ac56ab54c80fc1a5ff
SamG <127555+sam1am@users.noreply.github.com>
2024-01-21 20:53:23 -0700

    🚀feat(startup) enhance the start.sh script with git pull and absolute paths ⚙️chore(script) make start.sh more portable with APP_DIR variable

--- start.sh
+++ start.sh
@@ -1,7 +1,19 @@
-# check for venv folder, if it does not exist, create it
-if [ ! -d "venv" ]; then
-    python3 -m venv venv
-    ./venv/bin/pip install -r requirements.txt
+#!/bin/bash
+
+# Define the app's directory
+APP_DIR="/home/sam/poetroid"
+
+# Change to the app's directory
+cd $APP_DIR
+
+# Sync with git
+git pull
+
+# Check for venv directory, create it if it doesn't exist and install requirements
+if [ ! -d "$APP_DIR/venv" ]; then
+    python3 -m venv "$APP_DIR/venv"
+    "$APP_DIR/venv/bin/pip" install -r "$APP_DIR/requirements.txt"
 fi
 
-./venv/bin/python3 main_tk.py
+# Start application
+"$APP_DIR/venv/bin/python3" "$APP_DIR/main_tk.py"
