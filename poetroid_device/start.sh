44868dac307c6d2f2e9201cbcf903ac7f1caab6b
SamG <127555+sam1am@users.noreply.github.com>
2024-12-14 18:05:30 -0700

    🚀feat(poetroid-device) add support for python-dotenv 🛠️refactor(start-script) correct comment and improve virtualenv path handling

--- poetroid_device/start.sh
+++ poetroid_device/start.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# Get th current directory
+# Get the current directory
 APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PARENT_DIR="$(dirname "$APP_DIR")"
 
 # Sync with git
 git pull
 
 # Check for venv directory, create it if it doesn't exist and install requirements
-if [ ! -d "../$APP_DIR/venv" ]; then
-    python3 -m venv "../$APP_DIR/venv"
-    "../$APP_DIR/venv/bin/pip" install -r "../$APP_DIR/requirements.txt"
+if [ ! -d "$PARENT_DIR/venv" ]; then
+    python3 -m venv "$PARENT_DIR/venv"
+    "$PARENT_DIR/venv/bin/pip" install -r "$APP_DIR/requirements.txt"
 fi
 
 # Start application
-"../$APP_DIR/venv/bin/python3" "../$APP_DIR/poetroid_app.py"
+"$PARENT_DIR/venv/bin/python3" "$APP_DIR/poetroid_app.py"
