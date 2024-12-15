3710449b1a61b6f2bde715757425c3f6d71095b1
SamG <127555+sam1am@users.noreply.github.com>
2024-12-14 18:02:04 -0700

    🛠️refactor(poetroid_device) modify start script to use relative paths

--- poetroid_device/start.sh
+++ poetroid_device/start.sh
@@ -7,10 +7,10 @@ APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 git pull
 
 # Check for venv directory, create it if it doesn't exist and install requirements
-if [ ! -d "$APP_DIR/venv" ]; then
-    python3 -m venv "$APP_DIR/venv"
-    "$APP_DIR/venv/bin/pip" install -r "$APP_DIR/requirements.txt"
+if [ ! -d "../$APP_DIR/venv" ]; then
+    python3 -m venv "../$APP_DIR/venv"
+    "../$APP_DIR/venv/bin/pip" install -r "../$APP_DIR/requirements.txt"
 fi
 
 # Start application
-"$APP_DIR/venv/bin/python3" "$APP_DIR/poetroid_app.py"
+"../$APP_DIR/venv/bin/python3" "../$APP_DIR/poetroid_app.py"
